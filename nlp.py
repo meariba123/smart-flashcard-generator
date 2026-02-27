2fe2aa79612bb38a4284bc73c842c4603bde7d55
ariba.NET <ariba.1.naveed@gmail.com>
2026-02-27 21:33:31 +0000

    b

--- nlp.py
+++ nlp.py
@@ -11,9 +11,7 @@ from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
 
-# ----------------------------
-# LOAD SPACY
-# ----------------------------
+#LOAD SPACY
 try:
     nlp = spacy.load("en_core_web_sm")
 except:
